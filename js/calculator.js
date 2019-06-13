b2dde3fad0c29373ca3bd92e81ebe18c506b8e4d
crazykushu <34454011+crazykushu@users.noreply.github.com>
2019-06-13 11:57:36 -0700

    Final

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,15 @@
 function computeSaving() {
-                var coffee = document.getElementById('coffee').value * 3;
-                var burger = document.getElementById('burger').value * 8;
-                var pizza = document.getElementById('pizza').value * 20;
-                var movie = document.getElementById('movie').value * 14;
-                var totalMonth = coffee + burger + pizza + movie;
-                var totalYear = totalMonth * 12;
-                var result = 0;
-                for (i = 0; i < 30; i++)
-                {
-                    result = result + totalYear;
-                    result = (result * 1.07);
-                }
-                result = result.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                document.getElementById('result').innerHTML = "Your total saving after 30 years could be: $" + result;
-            }
+  var coffee = document.getElementById('coffee').value * 3;
+  var burger = document.getElementById('burger').value * 8;
+  var pizza = document.getElementById('pizza').value * 20;
+  var movie = document.getElementById('movie').value * 14;
+  var totalMonth = coffee + burger + pizza + movie;
+  var totalYear = totalMonth * 12;
+  var result = 0;
+  for (i = 0; i < 30; i++) {
+    result = result + totalYear;
+    result = (result * 1.07);
+  }
+  result = result.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById('result').innerHTML = "Your total saving after 30 years could be: $" + result;
+}
